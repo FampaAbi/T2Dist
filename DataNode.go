531c9f1fd12189c6dbd56669b896247abe1410a0
C-Becker1 <user@DESKTOP-07N8SSF.localdomain>
2020-12-02 00:11:02 -0300

    comunicación entre nodos

--- DataNode.go
+++ DataNode.go
@@ -13,7 +13,7 @@ import (
   "golang.org/x/net/context"
   "google.golang.org/grpc"
   pb "Tareita2/logistica"
-
+  
 )
 
 type Papi struct{
@@ -54,27 +54,90 @@ func(s *Papi) SayHello(ctx context.Context, message *pb.HelloRequest) (*pb.Hello
   return &pb.HelloReply{Mensaje: "Estoy disponible!"}, nil
 }
 
+func distribuirChunks(propuesta string[], libro byte[][], titulo string, address string, cantidad int) {
+  cuantas_cada_uno := len(libro) / len(propuesta)  
+  loquesobra := len(libro) % len(propuesta)
+  
+  for j := 0; j < len(propuesta) j++ {
+    for i := 0; i < cuantas_cada_uno; i++ {
+      if j == 0 && i == 0 {
+        EscribirEnNameNode(titulo,j+i,propuesta[j],cantidad,true)
+        EnviarChunk(propuesta[j],libro[j+i],titulo,j+i)
+      }else {
+        EscribirEnNameNode(titulo,j+i,propuesta[j],cantidad,false)
+        EnviarChunk(propuesta[j],libro[j+i],titulo,j+i)
+      }
+      //para cada dirección de la propuesta, se le mandan 'cuantas_cada_uno' chunks
+      //enviar a address 'j' 
+    }
+  } 
+}
+
+func EnviarChunk(address string, chunk byte[], titulo string, parte int) {
+  conn, err := grpc.Dial(address, grpc.WithInsecure())
+  if err != nil {
+    fmt.Println("did not connect: %v", err)
+  }
+  defer conn.Close
+  c := pb.NewLogisticaNameServiceClient(conn)
+  estadito, _ := c.MandarChunk(context.Background(), &pb.SendChunk{
+    //campos que se enviaran entre dataNodes
+    Titulo: titulo,
+    Chunk: chunk,
+    Parte: int32(parte),
+  })
+  fmt.Println("Recibido?:", estadito.GetRecibido())
+  }
+}
+
+func EscribirEnNameNode(titulo string, chunk int, address string, cantidad int, esPrimero bool)  {
+  conn, err := grpc.Dial("dist64:9000", grpc.WithInsecure())
+  if err != nil {
+    fmt.Println("did not connect: %v", err)
+  }
+  defer conn.Close
+  c := pb.NewLogisticaNameServiceClient(conn)
+  estadito, _ := c.MandarLog(context.Background(), &pb.LogMsg{
+    NombreLibro: titulo,
+    CantidadPartes: strconv.Itoa(cantidad),
+    Parte: strconv.Itoa(chunk),
+    IpMaquina: address,
+    EsPrimero: esPrimero,
+  })
+  fmt.Println("Recibido?:", estadito.GetStatus())
+  }
+}
+
 func(s *Papi) SubirLibro(ctx context.Context, dataLibro *pb.Libro) (*pb.SubirLibroReply,error){ // recibe info del libro y sus chunks
   len := 3
   i := len(dataLibro.GetChunks())
+  titulo := dataLibro.GetTitulo();
+  length := dataLibro.GetLength();
+  chunks := dataLibro.GetChunks();
   algoritmo := dataLibro.GetAlgoritmo() // 0:distribuido 1:centralizado
   address := dataLibro.GetIp() // ip de maquina actual
+
   if algoritmo == 1 {
-       prop := generarPropuesta(address,len)
-       conn, err := grpc.Dial("dist64:9000", grpc.WithInsecure())
-       if err != nil {
-         fmt.Println("did not connect: %v", err)
-       }
-       defer conn.Close()
+    prop := generarPropuesta(address,len)
+    conn, err := grpc.Dial("dist64:9000", grpc.WithInsecure())
+    if err != nil {
+      fmt.Println("did not connect: %v", err)
+    }
+    defer conn.Close()
+    
+    c := pb.NewLogisticaNameServiceClient(conn)
+    estadito, _ := c.MandarPropuestaName(context.Background(), &pb.PropuestaName{
+      Propuesta: prop,
+    })
+    fmt.Println("Respuesta:", estadito) // 1 : prop anterior 0: sacar nueva propuesta
+    if estadito.GetReplyName() == 1 {
+      distribuirChunks(prop, chunks, titulo, address, cantidad) 
+    } else {
+      distribuirChunks(estadito.GetNuevaProp(), chunks, titulo, address, cantidad)
+    }
 
-       c := pb.NewLogisticaNameServiceClient(conn)
-       estadito, _ := c.MandarPropuestaName(context.Background(), &pb.PropuestaName{
-         Propuesta: prop,
-       })
-       fmt.Println("Respuesta:", estadito) // 1 : prop anterior 0: sacar nueva propuesta
-
-  }else{ // algoritmo distribuido // si rechazan 2 prpuestas se acpeta siempre la 3ra que es dejar todo en este datanode
-
+  }else{ // algoritmo distribuido
+    continue
   }
   return &pb.SubirLibroReply{Status:int32(i)}, nil
 }
--- NameNode.go
+++ NameNode.go
@@ -15,24 +15,110 @@ import (
   pb "Tareita2/logisticaName"
 
 )
+type Papi struct{ //struct que maneja la info general de la logistica (EL PAPI)
+  libro int
+}
 
-type Papi struct{
+
+func remove(s []string, i int) []string { //borrar de un array https://yourbasic.org/golang/delete-element-slice/
+  s[len(s)-1], s[i] = s[i], s[len(s)-1]
+  return s[:len(s)-1]
+}
+
+func generarPropuesta(opciones []string, largo int) []string {
+  port := "9000" //
+  retorno = []
+  for i := 0; i < largo; i++ {
+    n_random := rand.Intn(largo-i)
+    random := opciones[n_random]
+    address := "dist" + strconv.Itoa(random) +":"+ port
+    opciones = remove(opciones, n_random)
+    retorno = append(retorno, address)
+  }
+  return retorno
+}
+
+func AceptaroRechazar() bool {
+  n_random= rand.Intn(100)
+  if n n_random < 20 {
+    return false
+  }
+  return true
 }
 
 func(s *Papi) MandarPropuestaName(ctx context.Context, propuesta *pb.PropuestaName) (*pb.ReplyPropuestaName,error){
-  n_random= rand.Intn(100)
-  res = 1
-  if n_random <20 {
-    res = 0
-    //generar una nueva propuesta y la reenvia
-    return &pb.HelloReply{ReplyName: 0, NuevaProp: nuevaProp}, nil
-  }else{
+  largo_propuesta := len(propuesta.GetPropuesta())  
+  believer := propuesta.GetPropuesta()
+
+  if largo_propuesta != 1 {
+    if AceptaroRechazar() {
+      var temp []string
+      temp = append(temp,"")
+      return &pb.HelloReply{ReplyName: 1 , NuevaProp: temp }, nil
+    } else {
+      if largo_propuesta > 2 {
+        nueva_prop := generarPropuesta(believer, 2)
+        return &pb.HelloReply{ReplyName: 0, NuevaProp: nueva_prop}, nil
+      }
+      else if largo_propuesta > 1 {
+        nueva_prop := generarPropuesta(believer, 1)
+        return &pb.HelloReply{ReplyName: 0, NuevaProp: nueva_prop}, nil
+      }
+    }  
+  } else {
     var temp []string
     temp = append(temp,"")
     return &pb.HelloReply{ReplyName: 1 , NuevaProp: temp }, nil
   }
 
+}
 
+func(s *Papi) MandarLog(ctx context.Context, LogMsg *pb.LogMsg) (*pb.ReplyLogMsg, error) {
+  
+  nombre_libro := LogMsg.GetNombreLibro()
+  cantidad_partes := LogMsg.GetCantidadPartes()
+  parte := LogMsg.GetParte()
+  ip := LogMsg.GetIpMaquina()
+  esPrimero := LogMsg.GetEsPrimero()
+  if esPrimero {
+    s.libro++
+  }
+
+  libro_actual := strconv.Itoa(s.libro)
+
+  escribirEnLog(nombre_libro,cantidad_partes, parte,ip, esPrimero, libro_actual)
+  return &pb.ReplyLogMsg{Recibido: true}, nil
+}
+
+func(s *Papi) MandarChunk(ctx context.Context, SendChunk *pb.SendChunk) (*pb.ReplySendChunk, error) {
+  titulo := SendChunk.GetTitulo() 
+  chunk := SendChunk.GetChunk()
+  parte  := SendChunk.GetParte()
+
+  _, err := os.Create("Partes/" + titulo)
+  if err != nil {
+		os.Exit(1)
+  }
+  ioutil.WriteFile("Partes/" + titulo, chunk, os.ModeAppend)
+
+  return &pb.ReplySendChunk{Status: true}, nil
+}
+
+
+func escribirEnLog(nombre_libro string, cantidad_partes string, parte string, ip string, esPrimero string, libro_actual string) {
+
+  file, err := os.OpenFile("./LOG.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Println(err)
+  }
+  if esPrimero {
+	  if _, err := file.WriteString(FileName + " " + strconv.Itoa(int(in.TotalParts)) + "\n"); err != nil {
+	  	log.Fatal(err)
+    }
+  }
+  if _, err := file.WriteString("parte_"+libro_actual+"_"+parte+" "+ip+"\n"); err != nil {
+    log.Fatal(err)
+  }
 }
 
 func main() {
@@ -43,7 +129,7 @@ func main() {
   }
 
   s := Papi{}
-
+  s.libro = 0
 
   grpcServer:= grpc.NewServer()
 
